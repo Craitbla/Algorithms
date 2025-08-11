18ed46fadb249b17f727ab94cf7926951f7bc913
Crait Blane <craitbla@va-f1.kzn.21-school.ru>
2025-08-11 12:57:17 +0300

    141 slow + fast

--- C++/leetcode/141.Linked_List_Cycle.cpp
+++ C++/leetcode/141.Linked_List_Cycle.cpp
@@ -11,6 +11,24 @@ struct ListNode {
   ListNode(int x) : val(x), next(NULL) {}
 };
 
+// fast и slow
+class Solution {
+public:
+  bool hasCycle(ListNode *head) {
+    ListNode *fast = head;
+    ListNode *slow = head;
+
+    while (fast && fast->next) {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
 // первый
 class Solution {
 public:
