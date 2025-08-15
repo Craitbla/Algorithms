be9dd621348176bc3f824b721127d9c584e6f2a0
Crait Blane <craitbla@va-f1.kzn.21-school.ru>
2025-08-15 17:09:34 +0300

    disguise begin

---
+++ Java/training mini tasks/2.2.Fullname_and_email/Disguise.java
@@ -0,0 +1,14 @@
+public class Disguise {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Вывод: Введите “1” для обработки ФИО и “2” для обработки email");
+        try {
+
+        } catch (Exception e) {
+            System.out.println();
+        }
+        scanner.close();
+    }
+
+}
